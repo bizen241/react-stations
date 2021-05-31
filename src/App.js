385bdd45dc4d5fbdfad960f87ba6e5ae7bad02e4
bizen241 <bizen241@gmail.com>
2021-06-01 01:16:07 +0900

    station07: add styles

--- src/App.js
+++ src/App.js
@@ -11,16 +11,40 @@ export const App = () => {
   const [dogUrl, setDogUrl] = React.useState('./dog1.jpg')
 
   const updateUrl = React.useCallback(async () => {
-    await fetch('https://dog.ceo/api/breeds/image/random')
-    setDogUrl('https://avatars.githubusercontent.com/u/298748')
+    try {
+      const response = await fetch('https://dog.ceo/api/breeds/image/random')
+      if (!response.ok) {
+        return
+      }
+
+      const json = await response.json()
+      const { message } = json
+
+      const url = new URL(message)
+      setDogUrl(url.toString())
+    } catch (e) {
+      console.error(e)
+    }
   }, [])
 
   return (
-    <div>
-      <header>Header</header>
-      <h2>Hello</h2>
-      <img src={dogUrl}></img>
-      <button onClick={updateUrl}>Button</button>
+    <div className="page__outer">
+      <header className="page-header__outer-wrapper">
+        <div className="page-header__inner-wrapper">
+          <div className="page-header__content">Dog App</div>
+        </div>
+      </header>
+      <div className="page-content__outer-wrapper">
+        <div className="page-content__inner-wrapper">
+          <h2 className="title">Image</h2>
+          <div className="image__wrapper">
+            <img className="image__content" src={dogUrl} alt="dog"></img>
+          </div>
+          <button className="button" onClick={updateUrl}>
+            Random
+          </button>
+        </div>
+      </div>
     </div>
   )
 }
